93876e9b9dfbfa6bb46ea2baee582b308a905970
rushikesh226 <rushikeshmali226@gmail.com>
2022-07-08 12:33:12 +0530

    counter with LS

--- Counter.js
+++ Counter.js
@@ -1,1 +1,17 @@
-console.log("File Created");
+document.querySelector("#inc").addEventListener("click",increaseCounter);
+    document.querySelector("#dec").addEventListener("click",decreaseCounter);
+
+    var counter=localStorage.getItem("counter")||0;
+    var cnt=document.querySelector("#counter");
+    cnt.innerText=counter;
+
+    function increaseCounter(){
+        counter++;
+        document.querySelector("#counter").innerText=counter;
+        localStorage.setItem("counter",counter);
+    }
+    function decreaseCounter(){
+        counter--;
+        document.querySelector("#counter").innerText=counter;
+        localStorage.setItem("counter",counter);
+    }
