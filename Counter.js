a7094a45b9b66b5c98c0f0fc1fac42ade2b48d76
rushikesh226 <rushikeshmali226@gmail.com>
2023-02-24 12:01:39 +0530

    counter

--- Counter.js
+++ Counter.js
@@ -1,17 +1,17 @@
-    document.querySelector("#inc").addEventListener("click",increaseCounter);
-    document.querySelector("#dec").addEventListener("click",decreaseCounter);
+document.querySelector("#inc").addEventListener("click", increaseCounter);
+document.querySelector("#dec").addEventListener("click", decreaseCounter);
 
-    var counter=localStorage.getItem("counter")||0;
-    var cnt=document.querySelector("#counter");
-    cnt.innerText=counter;
+var counter = localStorage.getItem("counter") || 0;
+var cnt = document.querySelector("#counter");
+cnt.innerText = counter;
 
-    function increaseCounter(){
-        counter++;
-        document.querySelector("#counter").innerText=counter;
-        localStorage.setItem("counter",counter);
-    }
-    function decreaseCounter(){
-        counter--;
-        document.querySelector("#counter").innerText=counter;
-        localStorage.setItem("counter",counter);
-    }
+function increaseCounter() {
+  counter++;
+  document.querySelector("#counter").innerText = counter;
+  localStorage.setItem("counter", counter);
+}
+function decreaseCounter() {
+  counter--;
+  document.querySelector("#counter").innerText = counter;
+  localStorage.setItem("counter", counter);
+}
